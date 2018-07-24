4ee3419879d2ab8cf3130f2f75f885c52595a6c7
rmit-s3652984-michael-guida <s3652984@student.rmit.edu.au>
2018-07-24 14:43:09 +1000

    Prediction added

--- app/src/main/java/com/example/diceout/MainActivity.java
+++ app/src/main/java/com/example/diceout/MainActivity.java
@@ -9,8 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,8 +21,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Random;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnItemSelectedListener {
+
+    //Player prediction
+    int[] prediction;
+
+    //Count for dice roll
+    int timesRolled = 0;
 
     // Field to hold the roll result text
     TextView rollResult;
@@ -34,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     int die1;
     int die2;
     int die3;
+    int die4;
 
     // ArrayList to hold all three die values
     ArrayList<Integer> dice;
@@ -43,6 +54,13 @@ public class MainActivity extends AppCompatActivity {
 
     //Field to hold the score text
     TextView scoreText;
+
+//    Button dicePred;
+
+    int spinner1Val;
+
+    private Spinner spinner1;
+    private static final String[]paths = {"1", "2", "3", "4", "5", "6"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                rollDice(view);
+                rollDice(view, timesRolled);
             }
         });
 
@@ -68,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
         // Link instances to widgets in the activity view
         rollResult = (TextView) findViewById(R.id.rollResult);
         scoreText = (TextView) findViewById(R.id.scoreText);
+//        dicePred = (Button) findViewById(R.id.button1);
 
         // Initialize the random number generator
         rand = new Random();
@@ -75,60 +94,175 @@ public class MainActivity extends AppCompatActivity {
         // Create ArrayList container for the dice values
         dice = new ArrayList<Integer>();
 
-        ImageView  die1Image = (ImageView) findViewById(R.id.die1Image);
-        ImageView  die2Image = (ImageView) findViewById(R.id.die2Image);
-        ImageView  die3Image = (ImageView) findViewById(R.id.die3Image);
+        ImageView die1Image = (ImageView) findViewById(R.id.die1Image);
+        ImageView die2Image = (ImageView) findViewById(R.id.die2Image);
+        ImageView die3Image = (ImageView) findViewById(R.id.die3Image);
+        ImageView die4Image = (ImageView) findViewById(R.id.die4Image);
 
         diceImageViews = new ArrayList<ImageView>();
         diceImageViews.add(die1Image);
         diceImageViews.add(die2Image);
         diceImageViews.add(die3Image);
+        diceImageViews.add(die4Image);
+
+        //Stores prediction
+        prediction = new int[4];
+
+
+        //On Action for dicePred
+//        dicePred.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                doSomething();
+//            }
+//        });
+
+
+
+
+
+        spinner1 = (Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter);
+        spinner1.setOnItemSelectedListener(this);
+
+
+
     }
 
-    public void rollDice(View v) {
-        // Roll dice
-        die1 = rand.nextInt(6)+1;
-        die2 = rand.nextInt(6)+1;
-        die3 = rand.nextInt(6)+1;
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
 
-        // Set dice values into an ArrayList
-        dice.clear();
-        dice.add(die1);
-        dice.add(die2);
-        dice.add(die3);
+        // Showing selected spinner item
+        //Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
 
-        for (int dieOfSet = 0; dieOfSet < 3; dieOfSet++){
-            String imageName = "die_" + dice.get(dieOfSet) + ".png";
+        //Gets value from spinner and converts it to int value to store in the predition array
+        prediction[0] = Integer.parseInt(spinner1.getSelectedItem().toString());
+
+        Toast.makeText(parent.getContext(), "Dice 1 Prediction: " + prediction[0], Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
 
 
-            try{
+
+
+//    public void doSomething(){
+//
+//        Toast.makeText(this, "Dice 1: You Predicted 1",Toast.LENGTH_SHORT).show();
+//    }
+
+    public void rollDice(View v, int count) {
+
+       // Toast.makeText(this, "Drop Val: " + dropVal,Toast.LENGTH_SHORT).show();
+        if (count == 0) {
+            die1 = rand.nextInt(6) + 1;
+            String imageName = "die_" + die1 + ".png";
+            try {
                 InputStream stream = getAssets().open(imageName);
                 Drawable d = Drawable.createFromStream(stream, null);
-                diceImageViews.get(dieOfSet).setImageDrawable(d);
-            }catch(IOException e){
+                diceImageViews.get(0).setImageDrawable(d);
+
+            } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            //Checks if prediction was correct
+            if (prediction[0] == die1){
+                Toast.makeText(this, "You predicted Dice 1 correctly",Toast.LENGTH_SHORT).show();
+            }
+            ++timesRolled;
         }
 
-        // Build message with the result
-        String msg;
+        if (count == 1) {
+            die2 = rand.nextInt(6) + 1;
+            String imageName = "die_" + die2 + ".png";
+            try {
+                InputStream stream = getAssets().open(imageName);
+                Drawable d = Drawable.createFromStream(stream, null);
+                diceImageViews.get(1).setImageDrawable(d);
 
-        if (die1 == die2 && die1 == die3){
-            //Tripple score
-            int scoreDelta = die1 * 100;
-            msg = "You rolled a tripple " + die1 + "! You score " + scoreDelta + " points!";
-            score += scoreDelta;
-        } else if (die1 == die2 || die1 == die3 || die2 == die3){
-            //Double score
-            msg = "You rolled a doubles for 50 points!";
-            score += 50;
-        } else {
-            msg = "You didn't score this roll, Try again!";
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //Checks if prediction was correct
+//            if (prediction.get(0) == die2){
+//                Toast.makeText(this, "You predicted correct",Toast.LENGTH_SHORT).show();
+//            }
+
+            ++timesRolled;
         }
 
-        // Update the app to display the result message
-        rollResult.setText(msg);
-        scoreText.setText("Score: " + score);
+        if (count == 2) {
+            die3 = rand.nextInt(6) + 1;
+            String imageName = "die_" + die3 + ".png";
+            try {
+                InputStream stream = getAssets().open(imageName);
+                Drawable d = Drawable.createFromStream(stream, null);
+                diceImageViews.get(2).setImageDrawable(d);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //Checks if prediction was correct
+//            if (prediction.get(0) == die3){
+//                Toast.makeText(this, "You predicted correct",Toast.LENGTH_SHORT).show();
+//            }
+
+            ++timesRolled;
+        }
+
+        if (count == 3) {
+            die4 = rand.nextInt(6) + 1;
+            String imageName = "die_" + die4 + ".png";
+            try {
+                InputStream stream = getAssets().open(imageName);
+                Drawable d = Drawable.createFromStream(stream, null);
+                diceImageViews.get(3).setImageDrawable(d);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String msg;
+            if (die1 == die2 && die2 == die3 && die3 == die4){
+                //Quad score
+                int scoreDelta = die1 * 200;
+                msg = "You rolled a quad " + die1 + "! You score " + scoreDelta + " points!";
+            }
+            else if (die1 == die2 && die1 == die3 || die2 == die3 && die3 == die4 || die1 == die3 && die3 == die4 || die1 == die2 && die1 == die4){
+                //Tripple score
+                int scoreDelta = die1 * 100;
+                msg = "You rolled a tripple " + die1 + "! You score " + scoreDelta + " points!";
+                score += scoreDelta;
+            } else if (die1 == die2 || die1 == die3 || die2 == die3){
+                //Double score
+                msg = "You rolled a doubles for 50 points!";
+                score += 50;
+            } else {
+                msg = "You didn't score this roll, Try again!";
+            }
+
+            // Update the app to display the result message
+            rollResult.setText(msg);
+            scoreText.setText("Score: " + score);
+
+            //Checks if prediction was correct
+//            if (prediction.get(0) == die4){
+//                Toast.makeText(this, "You predicted correct",Toast.LENGTH_SHORT).show();
+//            }
+
+            timesRolled = 0;
+        }
+
     }
 
     @Override
